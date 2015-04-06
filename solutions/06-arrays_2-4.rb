6d3cb265b0e86f088e93356df5129522d8a6cc81
Sven Fuchs <me@svenfuchs.com>
2015-04-06 21:44:43 +0200

    fix solution to exercise 06-arrays_2-4

--- solutions/06-arrays_2-4.rb
+++ solutions/06-arrays_2-4.rb
@@ -3,7 +3,7 @@
 <span class="n">words</span> <span class="o">=</span> <span class="n">words</span><span class="p">.</span><span class="nf">map</span> <span class="p">{</span> <span class="o">|</span><span class="n">word</span><span class="o">|</span> <span class="n">word</span><span class="p">.</span><span class="nf">capitalize</span> <span class="p">}</span>
 
 <span class="n">words</span> <span class="o">=</span> <span class="n">words</span><span class="p">.</span><span class="nf">map</span><span class="p">.</span><span class="nf">with_index</span> <span class="k">do</span> <span class="o">|</span><span class="n">word</span><span class="p">,</span> <span class="n">ix</span><span class="o">|</span>
-  <span class="n">hearts</span> <span class="o">=</span> <span class="s2">"&lt;3"</span> <span class="o">*</span> <span class="p">(</span><span class="n">ix</span> <span class="o">+</span> <span class="mi">1</span><span class="p">)</span>
+  <span class="n">hearts</span> <span class="o">=</span> <span class="s2">"&lt;3"</span> <span class="o">*</span> <span class="p">(</span><span class="n">ix</span> <span class="o">*</span> <span class="mi">2</span> <span class="o">+</span> <span class="mi">1</span><span class="p">)</span>
   <span class="s2">"</span><span class="si">#{</span><span class="n">word</span><span class="si">}</span><span class="s2"> </span><span class="si">#{</span><span class="n">hearts</span><span class="si">}</span><span class="s2">"</span>
 <span class="k">end</span>
 <span class="nb">p</span> <span class="n">words</span>
